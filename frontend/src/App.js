5c4553627d5ed02462508cb44112d8cd347a0758
DLee1993 <Dai180293@gmail.com>
2022-12-01 06:35:28 +0000

    added react-router routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
     return (
-        <>
+        <Router>
             <Header />
-            <main className="py-3">
+            <main className='py-3'>
                 <Container>
-                    <HomeScreen />
+                    <Routes>
+                        <Route exact path='/' element={<HomeScreen />} />
+                        <Route exact path='/product/:id' element={<ProductScreen />} />
+                    </Routes>
                 </Container>
             </main>
             <Footer />
-        </>
+        </Router>
     );
 };
 
