c6d7c62328bb453bfebd01cd9b9412bd4e1bcf0f
DLee1993 <Dai180293@gmail.com>
2022-12-01 07:49:30 +0000

    added the individual product page for each product

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 
-const ProductScreen = () => {
-  return (
-    <div>ProductScreen</div>
-  )
-}
+const ProductScreen = ({ match }) => {
+    const { id } = useParams();
+    const product = products.find((p) => p._id === id);
+    return (
+        <>
+            <Link className='btn btn-light my-3' to='/'>
+                Go Back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={5}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                          <Row>
+                            <Col md={9}>Price: ${product.price}</Col>
+                            <Col>{product.countInStock > 0 ? "In Stock" : "Out of Stock"}</Col>
+                          </Row>
+                        </ListGroup.Item>
+                        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                                Add to Cart
+                            </Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            </Row>
+        </>
+    );
+};
 
-export default ProductScreen
+export default ProductScreen;
