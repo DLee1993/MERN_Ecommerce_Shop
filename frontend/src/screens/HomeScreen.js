0291a9c205c9e743747dd4f61a7e5a65ab3c66e1
DLee1993 <Dai180293@gmail.com>
2022-12-02 00:51:36 +0000

    added the products list to the backend, added the axios calls to get the products or product, also added state

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,27 @@
-import React from "react";
-import products from "../products";
+// - useState is to use state in functional components
+// - useEffect is used to run as soon as the component loads
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
+    // - products is what we call the state
+    // - setProducts is what changes the state
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        // - anything placed in here will run as soon as the component loads
+        // - in order to use async in use effect you have to create a function
+        const fetchProducts = async () => {
+            // - make a call to get the products
+            const { data } = await axios.get("/products");
+            // - set the products state to the data from the axios call
+            setProducts(data);
+        };
+        fetchProducts();
+        // - you also need to pass in any dependencies for useEffect
+    }, []);
     return (
         <>
             <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,34 @@
-import React from "react";
+// - useState is to use state in functional components
+// - useEffect is used to run as soon as the component loads
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductScreen = ({ match }) => {
+    // - products is what we call the state
+    // - setProducts is what changes the state
+    const [product, setProduct] = useState({});
+
+    // - this gets the id we pass in when searching for a specific id
     const { id } = useParams();
-    const product = products.find((p) => p._id === id);
+
+    useEffect(() => {
+        // - anything placed in here will run as soon as the component loads
+        // - in order to use async in use effect you have to create a function
+        const fetchProduct = async () => {
+            // - make a call to get the products
+            const { data } = await axios.get(`/products/${id}`);
+            // - set the products state to the data from the axios call
+            setProduct(data);
+        };
+        fetchProduct();
+        // - you also need to pass in any dependencies for useEffect
+        // - id is needed as a dependancy as it changes depending on the product
+        // - useEffect will need to run when the id changes to get the correct product
+    }, [id]);
+
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>
@@ -25,14 +47,20 @@ const ProductScreen = ({ match }) => {
                             <Rating value={product.rating} text={`${product.numReviews} reviews`} />
                         </ListGroup.Item>
                         <ListGroup.Item>
-                          <Row>
-                            <Col>Price: ${product.price}</Col>
-                            <Col style={{textAlign: 'right'}}>{product.countInStock > 0 ? "In Stock" : "Out of Stock"}</Col>
-                          </Row>
+                            <Row>
+                                <Col>Price: ${product.price}</Col>
+                                <Col style={{ textAlign: "right" }}>
+                                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                                </Col>
+                            </Row>
                         </ListGroup.Item>
                         <ListGroup.Item>Description: {product.description}</ListGroup.Item>
                         <ListGroup.Item>
-                            <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                            <Button
+                                className='btn-block'
+                                type='button'
+                                disabled={product.countInStock === 0}
+                            >
                                 Add to Cart
                             </Button>
                         </ListGroup.Item>
