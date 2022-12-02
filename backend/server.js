c54456df731c19813ae8beb888c9256fc127eb7b
DLee1993 <Dai180293@gmail.com>
2022-12-02 17:19:43 +0000

    changed server.js to es6 modules, added nodemon and concurrently for dev dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
-const express = require("express"),
-    app = express(),
-    products = require("./data/products");
+import express from "express";
+import dotenv from 'dotenv'; 
+import products from './data/products.js'   
+const PORT = process.env.PORT || 5000; 
+
+dotenv.config(); 
+const app = express()
 
 app.get("/", (req, res) => {
     res.send("Api is running");
 });
 
 app.get("/products", (req, res) => {
-    res.json(products)
+    res.json(products);
 });
 
 app.get("/products/:id", (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
