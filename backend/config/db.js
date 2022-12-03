6d08eb161b87de6e4e705d1d4ec3b97e1e3beffb
DLee1993 <Dai180293@gmail.com>
2022-12-03 09:25:49 +0000

    added colors package

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose, { connect } from "mongoose";
 const connectDatabase = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Database is connected`);
+        console.log(`Database is connected`.bgCyan.bold);
     } catch (error) {
-        console.log(`Error: ${error}`);
+        console.log(`Error: ${error}`.red.underline.bold);
         process.exit(1);
     }
 };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv'; 
 import connectDatabase from "./config/db.js";
 import products from './data/products.js'   
+import colors from 'colors'; 
 const PORT = process.env.PORT || 5000; 
 
 dotenv.config(); 
@@ -21,4 +22,4 @@ app.get("/products/:id", (req, res) => {
     res.json(product);
 });
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgYellow.bold));
