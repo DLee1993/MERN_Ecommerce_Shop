4219509ed6e6dd973ce63d4ad2be8ee399f5d67c
DLee1993 <Dai180293@gmail.com>
2022-12-05 10:36:37 +0000

    added the routes to fetch data from the database

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
 import express from "express";
-import dotenv from 'dotenv'; 
+import dotenv from "dotenv";
 import connectDatabase from "./config/db.js";
-import products from './data/products.js'   
-import colors from 'colors'; 
-const PORT = process.env.PORT || 5000; 
+import productRoutes from "./routes/productRoutes.js";
+import colors from "colors";
+const PORT = process.env.PORT || 5000;
 
-dotenv.config(); 
-const app = express(); 
-connectDatabase()
+dotenv.config();
+const app = express();
+connectDatabase();
 
 app.get("/", (req, res) => {
     res.send("Api is running");
 });
 
-app.get("/products", (req, res) => {
-    res.json(products);
-});
+// - this is using seperate routes ( productRoutes.js )
+// - all of these routes will start will /products
+app.use('/products', productRoutes)
 
-app.get("/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgYellow.bold));
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgYellow.bold)
+);
