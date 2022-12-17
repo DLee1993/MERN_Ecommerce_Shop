bb4478c0d5d5b48184017e3162215fa04967da5c
DLee1993 <Dai180293@gmail.com>
2022-12-17 07:00:30 +0000

    added the placeOrder route and controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,39 @@
+import Order from "../models/OrderModel.js";
+
+// - Description ( Create a new order )
+// - Route ( /orders )
+// - Request Type ( POST )
+// - Authentication ( Private route - authentication needed )
+const addOrderItems = async (req, res) => {
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error("no order items");
+    } else {
+        const order = new Order({
+            user: req.user._id,
+            orderItems,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+
+        const createdOrder = await order.save();
+
+        res.status(201).json(createdOrder);
+    }
+};
+
+export { addOrderItems };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDatabase from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import colors from "colors";
 const PORT = process.env.PORT || 5000;
@@ -24,6 +25,11 @@ app.use("/products", productRoutes);
 // - this is using seperate routes ( userRoutes.js )
 // - all of these routes will start will /users
 app.use("/users", userRoutes);
+
+
+// - this is using seperate routes ( orderRoutes.js )
+// - all of these routes will start will /users
+app.use("/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
