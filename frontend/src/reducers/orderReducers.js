d77f2fb082bbd45759d8506e47b07a9fa35d0d15
DLee1993 <Dai180293@gmail.com>
2022-12-17 20:57:07 +0000

    added the orderDetails reducer, constants and action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,6 +2,9 @@ import {
     ORDER_CREATE_REQ,
     ORDER_CREATE_SUCCESS,
     ORDER_CREATE_FAIL,
+    ORDER_DETAILS_REQ,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -12,6 +15,20 @@ export const orderCreateReducer = (state = {}, action) => {
         case ORDER_CREATE_SUCCESS:
             return { loading: false, success: true, order: payload };
         case ORDER_CREATE_FAIL:
+            return { loading: false, error: payload };
+        default:
+            return { ...state };
+    }
+};
+
+export const orderDetailsReducer = (state = { orderItems: [], shippingAddress: {} }, action) => {
+    const { payload } = action;
+    switch (action.type) {
+        case ORDER_DETAILS_REQ:
+            return { ...state, loading: true };
+        case ORDER_DETAILS_SUCCESS:
+            return { loading: false, order: payload };
+        case ORDER_DETAILS_FAIL:
             return { loading: false, error: payload };
         default:
             return { ...state };
