5bd36e4e91293b3c248674f90508fd7d6b9133dd
DLee1993 <Dai180293@gmail.com>
2022-12-18 05:29:34 +0000

    updated the details button link

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -157,7 +157,7 @@ const ProfileScreen = () => {
                                             )}
                                         </td>
                                         <td>
-                                            <LinkContainer to={`/order/${order._id}`}>
+                                            <LinkContainer to={`/orders/${order._id}`}>
                                                 <Button className='btn-sm' variant='light'>Details</Button>
                                             </LinkContainer>
                                         </td>
